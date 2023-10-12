adea2008e96026680033fb49cc28bc4b804d93c2
Bram Steur <220507@student.glu.nl>
2023-10-12 09:54:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GLU X PSF</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <header>
+        <div class="header">
+            <img src="assets/img/blauwlogo.png" alt="logo">
+           <div>
+                <h1>GLU BUREAU X Plastic Soup foundation</h1>
+                <div class="team">Medemogelijk gamaakt door team: <ab id="blauw">BLAUW<ab></div>
+            </div> 
+            <div id="floatHeader"><button type="button"><a href="https://www.plasticsoupfoundation.org/doneren-goed-doel-plasticsoep/">Doneer NU!</a></button></div>
+        </div>
+    </header>
+    <div class="quote">
+        <h3 id="smallquote">Onze skills helpen anderen</h3>
+        <h2>KIES HET BUREAU!</h2>
+        <h3 id="defender">Doneer en word een: Ocean Defender.</h3>
+    </div>
+    <img src="assets/img/tekening.jpg" alt="illustratie" id="illustratie">
+    <div class="tiktok">
+        <video width="1450" height="" controls>
+            <source src="assets/videos/tiktokvid.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+         </video>
+    </div>
+    <div class="gallerij">
+        <img class="posters" src="assets/img/poster1.png" alt="poster1">
+        <img class="posters" src="assets/img/poster2.png" alt="poster2">
+        <img src="assets/img/poster3.png" alt="poster3" class="posters">
+    </div>
+    <footer>
+        <div class="footer">
+            <div>
+                <h3>GLU BUREAU X Plastic Soup foundation</h3><br>
+                <h4>Onze skills helpen anderen!</h4>
+            </div>
+            <p>&copyGLUxPSF</a> </p>
+        </div>
+    </footer>
+</body>
+</html>
